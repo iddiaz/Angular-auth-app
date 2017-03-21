c0657b8e18fa2bcc0f162cee092fb7b75a16f8c5
iddiaz <diazdiazivan@gmail.com>
2017-03-21 13:25:59 +0100

    Instalación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { Auth } from './services/auth.service';
+import { APP_ROUTING } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [Auth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// "../node_modules/bootstrap/dist/css/bootstrap.min.css",
+
+    // "../node_modules/jquery/dist/jquery.slim.min.js",
+        // "../node_modules/tether/dist/js/tether.min.js",
+        // "../node_modules/bootstrap/dist/js/bootstrap.min.js"
