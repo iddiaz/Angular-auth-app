c2bcf8c59937af7aed4e5dcaeff230518e9b6caf
iddiaz <diazdiazivan@gmail.com>
2017-03-21 14:44:20 +0100

    Solucion de error jwt con versiones de angular-cli recientes
    esto es una modificación en la forma de usar el módulo de jwt, por si es necesario, para uqe no de errores en la carga del servidor.
    
    FIX: jwt error in new version angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
+
 
 //routing
 import { APP_ROUTING } from './app.routes';
 
 //validation
 import { Auth } from './services/auth.service';
-import { AUTH_PROVIDERS } from 'angular2-jwt';
+
+//Solucion de error para versiones actuales de angular-cli:
+//Podemos comentar el modulo importado ya que en la aplicación actualmente no se está usando y listo.
+// import { AUTH_PROVIDERS } from 'angular2-jwt';
+
+// O usarlo de esta forma:
+import { HttpModule, Http, RequestOptions } from '@angular/http';
+
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
+
+export function authHttpServiceFactory( http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options );
+}
 
 //components
 import { AppComponent } from './app.component';
@@ -31,7 +44,14 @@ import { ProtegidaComponent } from './components/protegida/protegida.component';
     HttpModule,
     APP_ROUTING
   ],
-  providers: [Auth, AUTH_PROVIDERS],
+  providers: [Auth,
+  // AUTH_PROVIDERS,
+  {
+    provide: AuthHttp,
+    useFactory: authHttpServiceFactory,
+    deps: [ Http, RequestOptions ]
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
